552373bf1eb5fd4b165178db7492f997fa759fba
JohanMendoza29 <johanmendoza29777@gmail.com>
2024-11-28 22:14:11 -0600

    Agregué un boton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Location = new System.Drawing.Point(198, 271);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(130, 47);
+            this.btn_Ingresar.TabIndex = 0;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Ingresar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,7 @@ namespace ProyectoProgramacion4
         {
             InitializeComponent();
 
-        }hola johan fggdd
+        }
+       
     }
 }
