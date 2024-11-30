13adada6bc9f2df02bda1a741a764d23060104f6
daik2004 <daisandif@gmail.com>
2024-11-29 23:57:40 -0600

    cuidado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,11 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 253);
+            this.ClientSize = new System.Drawing.Size(779, 416);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
-         
             this.ResumeLayout(false);
 
         }
