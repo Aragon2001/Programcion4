578fb11636e14a4b3d47f5b55c526b399c35fc7d
daik2004 <daisandif@gmail.com>
2024-11-29 23:46:02 -0600

    Creacion de Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,38 +36,42 @@
             // btn_Ingresar
             // 
             this.btn_Ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Ingresar.Location = new System.Drawing.Point(199, 297);
+            this.btn_Ingresar.Location = new System.Drawing.Point(149, 230);
+            this.btn_Ingresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Ingresar.Name = "btn_Ingresar";
-            this.btn_Ingresar.Size = new System.Drawing.Size(141, 30);
+            this.btn_Ingresar.Size = new System.Drawing.Size(106, 24);
             this.btn_Ingresar.TabIndex = 0;
             this.btn_Ingresar.Text = "Ingresar";
             this.btn_Ingresar.UseVisualStyleBackColor = true;
             // 
             // txt_Cedula
             // 
-            this.txt_Cedula.Location = new System.Drawing.Point(194, 159);
+            this.txt_Cedula.Location = new System.Drawing.Point(146, 129);
+            this.txt_Cedula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Cedula.Name = "txt_Cedula";
-            this.txt_Cedula.Size = new System.Drawing.Size(141, 22);
+            this.txt_Cedula.Size = new System.Drawing.Size(107, 20);
             this.txt_Cedula.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(195, 125);
+            this.label1.Location = new System.Drawing.Point(146, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.Size = new System.Drawing.Size(58, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Cedula";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 402);
+            this.ClientSize = new System.Drawing.Size(405, 327);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Cedula);
             this.Controls.Add(this.btn_Ingresar);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
